caf340dbdeff03077a889a2a1bd05cee6f522cf5
thebignet <thebignet@gmail.com>
2014-07-04 22:57:41 +0200

    Répéteurs

--- js/piouter.js
+++ js/piouter.js
@@ -6,4 +6,9 @@ app.controller('MainCtrl', function ($scope, $log, userId) {
     $scope.message = 'Message';
     $scope.newUser = '';
     $scope.userToFollow = '';
+    $scope.pious = [
+        {id:'thebignet@gmail.com',message:'Salut les gens',date:new Date()},
+        {id:'machin@gmail.com',message:'Bonjour',date:new Date()},
+        {id:'thebignet@gmail.com',message:'Kikou lol',date:new Date()}
+    ];
 });
